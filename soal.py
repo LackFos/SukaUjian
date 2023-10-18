48b293187f8a3aa715306a4a583fbc8720e2e788
LackFos <80957646+LackFos@users.noreply.github.com>
2023-10-18 23:04:07 +0700

    Some update feature

--- soal.py
+++ soal.py
@@ -6,11 +6,15 @@ class soal:
     
     def __inputExamId(self):
         # Dapatkan id exam
-        print("\nPilih matakuliah\n")
+        print("\nPilih exam\n")
         print("1. Matematika\n2. Bahasa Indonesia\n3. Mandarin")
         return input("\nMasukkan id nomor matakuliah : ")
 
     def __printTable(self, data):
+        if(len(data) == 0):
+            print("\nTidak ada soal pada exam yang dipilih")
+            return False;
+
         headers = data[0].keys()
 
         column_widths = {header: len(str(header)) for header in headers}
@@ -24,30 +28,49 @@ class soal:
         for row in data:
             print("|".join(f"{str(row[header]):<{column_widths[header]}}" for header in headers))
 
+        return True
 
     def tambahSoal(self):
-        # Dapatkan id exam
-        examId = self.__inputExamId()
+        soalTidakKosong = self.dapatkanSoal()
 
-        # Dapatkan soal
-        soalBaru = input("Input soal : ")
+        if(not soalTidakKosong):
+            return
 
         # Dapatkan nomor soal terakhir
-        nomorSoalTerakhir = self.__db.rawOne(f"SELECT * FROM soal WHERE exam_id = %s ORDER BY nomor DESC LIMIT 1", [examId])["nomor"]
+        soalTerakhir = self.__db.rawOne(f"SELECT * FROM soal WHERE exam_id = %s ORDER BY nomor DESC LIMIT 1", [self.examId])
+        nomorSoal = nomorSoalTerakhir["nomor"] + 1 if soalTerakhir else 1
 
-        self.__db.insert("soal", {"exam_id": examId, "nomor": nomorSoalTerakhir + 1, "soal": soalBaru})
+        self.__db.insert("soal", {"exam_id": self.examId, "nomor": nomorSoal, "soal": soalBaru})
         
     def hapusSoal(self):
-        # Dapatkan id exam
-        examId = self.__inputExamId()
+        soalTidakKosong = self.dapatkanSoal()
 
-        # print list soal
-        listSoal = self.__db.get("soal", {"exam_id": examId})
-        self.__printTable(listSoal)
+        if(not soalTidakKosong):
+            return
 
         # Dapatkan nomor soal yang ingin dihapus
-        soalBaru = input("\nMasukkan nomor soal yang ingin dihapus : ")
+        nomorSoal = input("\nMasukkan nomor soal yang ingin dihapus : ")
+
+        self.__db.delete("soal", {"exam_id": self.examId, "nomor": nomorSoal})
+
+    def editSoal(self):
+        soalTidakKosong = self.dapatkanSoal()
+
+        if(not soalTidakKosong):
+            return
+
+        # Dapatkan nomor soal yang ingin dihapus
+        nomorSoal = input("\nMasukkan nomor soal yang ingin diganti : ")
+        soalBaru = input("\nInput soal baru : ")
+        self.__db.update("soal", {"exam_id": self.examId, "nomor": nomorSoal}, {"soal": soalBaru})
+
+    def dapatkanSoal(self, isRandom=False):
+        self.examId = self.__inputExamId()
+
+        # print list soal
+        listSoal = self.__db.get("soal", {"exam_id": self.examId}, ["soal"]) if not isRandom else self.__db.raw("SELECT soal FROM soal ORDER BY RAND()")
+        return self.__printTable(listSoal)
 
 
 soal = soal()
-soal.hapusSoal()
+soal.dapatkanSoal(True)
