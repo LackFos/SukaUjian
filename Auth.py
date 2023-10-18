09dd3d409ff57090b647723e160b6aa2d9d69123
WilsenL <93707937+WilsenL@users.noreply.github.com>
2023-10-18 18:34:24 +0700

    Update Auth.py

--- Auth.py
+++ Auth.py
@@ -13,38 +13,49 @@ Conn = Connect()
 
 
 class Auth():
+    def __init__(self, user, passw):
+        self.user = user
+        self.__passw = passw
+
     def login(self):
         user = Conn.first("pbo", {"Name": a})
         passw = Conn.first("pbo", {"Password": b})
-        print(user, passw)
+        if user and passw is not None:
+            print("Login Succses")
+            Auth.logout(main)
+        else:
+            print("Login Fail")
 
     def logout(self):
-        if self.__successlogin == True:
-            print("Apakah %s Mau Keluar?" % self.user)
-            keluar = input("1. Yes\n2. No\n")
-            if keluar == "1":
-                self.user = ""
-                self.__passw = ""
-                print("Berhasil Logout")
-            else:
-                print("Silahkan melanjutkan")
+        print("Apakah %s Mau Keluar?" % self.user)
+        keluar = input("1. Yes\n2. No\n")
+        if keluar == "1":
+            self.user = ""
+            self.__passw = ""
+            print("Berhasil Logout")
         else:
-            return 0
+            print("Silahkan melanjutkan")
 
 
-main = input("Do you want to login / register?\n1. Login\n2.Register")
+main = input(
+    "What do you wanat to do\n1. Login\n2.Register\n3.Forget Password\n")
 
-if main == '1':
-    a = input("Masukan Username\n")
-    b = input("Masukan Password\n")
-    main = Auth()
-    Auth.login(main)
-    # Auth.logout(main)
-else:
-    a = input("Masukan Username Baru\n")
-    b = input("Masukan Password Baru\n")
-    # Conn.insert("user", {"Name": a, "Password": b})
-    # Conn.update("user", {"Name": a}, {"Password": b})
-    # Conn.delete("user", {"Name": a})
+match main:
+    case "1":
+        a = input("Masukan Username\n")
+        b = input("Masukan Password\n")
+        main = Auth(a, b)
+        Auth.login(main)
+    case "2":
+        a = input("Masukan Username Baru\n")
+        b = input("Masukan Password Baru\n")
+        Conn.insert("pbo", {"ID": None, "Name": a, "Password": b})
+    case "3":
+        pert = input("Masukan Username\n")
+        pert2 = input("Masukan Password Baru\n")
+        Conn.update("pbo", {"Name": pert}, {"Password": pert2})
 
-    print("done")
+
+#     # Conn.delete("user", {"Name": a})
+
+#     print("done")
