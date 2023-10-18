fa14986cb96e2d74ee2aca818c4a0746146b2574
Fariz1703 <146436715+Fariz1703@users.noreply.github.com>
2023-10-18 17:51:57 +0700

    antar

--- Dosen.py
+++ Dosen.py
@@ -4,55 +4,58 @@ from Database.Connect import Connect
 class Dosen:
     def __init__(self,id):
         self.db=Connect()
-        result=self.db.get('dosen', {"ID": id})
-        self.id=result.get("ID")
-        self.name=result.get("Name")
-        self.courses=result.get("Courses")
+        Result=self.db.first('dosen',{"ID":id})
+        self.id=Result["ID"]
+        self.nama=Result["NAMA"]
+        self.matakuliah=Result["MATAKULIAH"]
 
     def ExamSchedule(self):
-        print(f"Halo MR/MRS {self.name}")
+        print(f"Halo MR/MRS {self.nama}")
         Date=input("Silakan masukan tanggal ujian(YYYY-MM-DD): ")
         Time=input("Silakan masukan Waktu ujian(HH:MM-HH:MM): ")
         self.db.insert(
-            'Ujian',
+            'jadwal',
             {"ID":"Null", 
-            "NamaDosen":f"{self.name}",
-            "MataKuliah":f"{self.courses}",
-            "Jam":f"{Time}",
-            "Tanggal":f"{Date}"}
+            "DOSEN":f"{self.nama}",
+            "MATAKULIAH":f"{self.matakuliah}",
+            "JAM":f"{Time}",
+            "TANGGAL":f"{Date}"}
             )
         
 
         
     def DisplayExam(self):
-        x=self.db.select("ujian")
+        x=self.db.select("jadwal")
         for i in range(len(x)):
             print(f"{i+1}. {x[i]}")
 
 
     def UpdateExam(self):
         self.DisplayExam()
-        print(f"Halo MR/MRS {self.name}, Silakan melakukan perubahan jadwal dimohon untuk tidak mengaganngu jadwal dosen lain")
+        print(f"Halo MR/MRS {self.nama}, Silakan melakukan perubahan jadwal dimohon untuk tidak mengaganngu jadwal dosen lain")
         x=input("Silakan masukan ID: ")
         Date=input("Silakan masukan tanggal ujian(YYYY-MM-DD): ")
         Time=input("Silakan masukan Waktu ujian(HH:MM-HH:MM): ")
         self.db.update(
-            "ujian",
+            "jadwal",
             {"ID":f"{x}"},
-            {"Jam":f"{Time}",
-            "Tanggal":f"{Date}"}
+            {"JAM":f"{Time}",
+            "TANGGAL":f"{Date}"}
         )
 
     def DeleteExam(self):
         self.DisplayExam()
-        print(f"Halo MR/MRS {self.name}, Silakan melakukan penghapusan jadwal dimohon untuk tidak mengaganngu jadwal dosen lain")
+        print(f"Halo MR/MRS {self.nama}, Silakan melakukan penghapusan jadwal dimohon untuk tidak mengaganngu jadwal dosen lain")
         x=input("Silakan masukan ID: ")
-        self.db.delete("ujian",{"ID":f"{x}"})
+        self.db.delete("jadwal",{"ID":f"{x}"})
 
 
 
 dosen1=Dosen(1)
-#dosen1.ExamSchedule()
+dosen2=Dosen(2)
+dosen1.ExamSchedule()
+dosen2.ExamSchedule()
+
 #dosen1.DisplayExam()
 #dosen1.UpdateExam()
-dosen1.DeleteExam()
+#dosen1.DeleteExam()
--- dosentest.py
+++ dosentest.py
@@ -1,6 +1,11 @@
+import mysql.connector
 from Database.Connect import Connect
 
-db=Connect()
-result=db.get('ujian', {"Matakuliah": "Matematika"})
-y=result.get("MataKuliah")
-print(y)
+class Dosen:
+    def __init__(self,id):
+        self.db=Connect()
+        Result=self.db.first('dosen',{"ID":id})
+        self.id=Result["ID"]
+
+dosen1=Dosen(1)
+print(dosen1.id)
