bdfba21f9381a3d9cbf0b5aae6de3afff2c4b8fe
Fariz1703 <146436715+Fariz1703@users.noreply.github.com>
2023-10-24 19:24:07 +0700

    final code

--- Dosen.py
+++ Dosen.py
@@ -1,11 +1,11 @@
 import mysql.connector
 from Database.Connect import Connect
 
-class Dosen:
+class Jadwal:
     def __init__(self,id):
-        self.db=Connect()
-        Result=self.db.first('dosen',{"ID":id})
-        self.id=Result["ID"]
+        self.__db=Connect()
+        Result=self.__db.first('dosen',{"ID":id})
+        self.__id=Result["ID"]
         self.nama=Result["NAMA"]
         self.matakuliah=Result["MATAKULIAH"]
 
@@ -13,7 +13,7 @@ class Dosen:
         print(f"Halo MR/MRS {self.nama}")
         Date=input("Silakan masukan tanggal ujian(YYYY-MM-DD): ")
         Time=input("Silakan masukan Waktu ujian(HH:MM-HH:MM): ")
-        self.db.insert(
+        self.__db.insert(
             'jadwal',
             {"ID":"Null", 
             "DOSEN":f"{self.nama}",
@@ -25,7 +25,7 @@ class Dosen:
 
         
     def DisplayExam(self):
-        x=self.db.select("jadwal")
+        x=self.__db.select("jadwal")
         for i in range(len(x)):
             print(f"{i+1}. {x[i]}")
 
@@ -36,7 +36,7 @@ class Dosen:
         x=input("Silakan masukan ID: ")
         Date=input("Silakan masukan tanggal ujian(YYYY-MM-DD): ")
         Time=input("Silakan masukan Waktu ujian(HH:MM-HH:MM): ")
-        self.db.update(
+        self.__db.update(
             "jadwal",
             {"ID":f"{x}"},
             {"JAM":f"{Time}",
@@ -47,15 +47,33 @@ class Dosen:
         self.DisplayExam()
         print(f"Halo MR/MRS {self.nama}, Silakan melakukan penghapusan jadwal dimohon untuk tidak mengaganngu jadwal dosen lain")
         x=input("Silakan masukan ID: ")
-        self.db.delete("jadwal",{"ID":f"{x}"})
+        self.__db.delete("jadwal",{"ID":f"{x}"})
+
+while True:
+    try:
+        id=input("Silakan masukan id dosen: ")
+        dosen=Jadwal(id)
+        break
+    except:
+        print("Input eror masukan id dosen dengan benar")
+
+while True:
+    y=["Buat Jadwal","Update Jadwal", "Melihat Jadwal", "Hapus Jadwal", "Keluar"]
+    for i in range(len(y)):
+        print(f"{i+1}. {y[i]}")
+    x=input(f"Halo mr/mrs {dosen.nama} silakan masukan input nomor: ")
+    if x == "1":
+        dosen.ExamSchedule()
+    elif x == "2":
+        dosen.UpdateExam()
+    elif x == "3":
+        dosen.DisplayExam()
+    elif x == "4":
+        dosen.DeleteExam()
+    elif x == "5":
+        print("Terimakasih")
+        break
+    else:
+        print("Input eror silakan masukan ulang")
 
 
-
-dosen1=Dosen(1)
-dosen2=Dosen(2)
-dosen1.ExamSchedule()
-dosen2.ExamSchedule()
-
-#dosen1.DisplayExam()
-#dosen1.UpdateExam()
-#dosen1.DeleteExam()
