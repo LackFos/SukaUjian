086bd0c8134c9b73830f9adc656d914b341f4be6
LackFos <80957646+LackFos@users.noreply.github.com>
2023-11-27 20:20:33 +0700

    UI Update

--- ui/home.py
+++ ui/home.py
@@ -1,6 +1,9 @@
 from customtkinter import *
 from ujian import UjianPage
 from nilai import NilaiPage
+from soal import SoalPage
+from kelas import KelasPage
+from jadwal import JadwalPage
 from PIL import Image, ImageTk
 
 class HomePage(CTkFrame):
@@ -19,22 +22,37 @@ class HomePage(CTkFrame):
         logo = CTkLabel(sidebar, image=logoImage, text="")
         logo.grid(row=0, column=0, padx=24, pady=(24,16))
 
-        tombolUjianLogo = CTkImage(Image.open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'assets', 'ujianicon.png')), size=(24, 24))
-        tombolUjian = CTkButton(sidebar, image=tombolUjianLogo, text="Ujian",  command=lambda: self.navigateTo("ujian"))
-        tombolUjian.configure(height=48, font=("Poppins", 16, "bold"), anchor="w")
-        tombolUjian.grid(row=1, column=0, padx=16, pady=(24, 0), sticky="ew")
+        l1 = CTkImage(Image.open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'assets', 'ujianicon.png')), size=(24, 24))
+        t1 = CTkButton(sidebar, image=l1, text="Ujian",  command=lambda: self.navigateTo("ujian"))
+        t1.configure(height=48, font=("Poppins", 16, "bold"), anchor="w")
+        t1.grid(row=1, column=0, padx=16, pady=(24, 0), sticky="ew")
 
-        tombolNilaiLogo = CTkImage(Image.open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'assets', 'nilaiicon.png')), size=(24, 24))
-        tombolNilai = CTkButton(sidebar, image=tombolNilaiLogo, text="Nilai", command=lambda: self.navigateTo("nilai"))
-        tombolNilai.configure(height=48, font=("Poppins", 16, "bold"), anchor="w")
-        tombolNilai.grid(row=2, column=0, padx=16, pady=(16), sticky="ew")
+        l2 = CTkImage(Image.open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'assets', 'nilaiicon.png')), size=(24, 24))
+        t2 = CTkButton(sidebar, image=l2, text="Nilai", command=lambda: self.navigateTo("nilai"))
+        t2.configure(height=48, font=("Poppins", 16, "bold"), anchor="w")
+        t2.grid(row=2, column=0, padx=16, pady=(16,0), sticky="ew")
     
+        t3 = CTkButton(sidebar, text="Soal", command=lambda: self.navigateTo("soal"))
+        t3.configure(height=48, font=("Poppins", 16, "bold"), anchor="w")
+        t3.grid(row=3, column=0, padx=16, pady=(16,0), sticky="ew")
+
+        t4 = CTkButton(sidebar, text="Kelas", command=lambda: self.navigateTo("kelas"))
+        t4.configure(height=48, font=("Poppins", 16, "bold"), anchor="w")
+        t4.grid(row=4, column=0, padx=16, pady=(16,0), sticky="ew")
+
+        t5 = CTkButton(sidebar, text="Jadwal", command=lambda: self.navigateTo("jadwal"))
+        t5.configure(height=48, font=("Poppins", 16, "bold"), anchor="w")
+        t5.grid(row=5, column=0, padx=16, pady=(16,0), sticky="ew")
+
         content = CTkFrame(self, fg_color='white', corner_radius=8)
         content.grid(row=0, column=1, sticky="nsew", pady=8, padx=8)
         
         self.pages = {
             "ujian": UjianPage(content),
-            "nilai": NilaiPage(content)
+            "nilai": NilaiPage(content),
+            "kelas": KelasPage(content),
+            "soal": SoalPage(content),
+            "jadwal": JadwalPage(content)
         }
         
         self.navigateTo("ujian")
