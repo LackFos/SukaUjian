03fdaac500a724f6a0b93d5ccca438b4a5ec1dc0
Fariz1703 <146436715+Fariz1703@users.noreply.github.com>
2023-11-30 23:48:13 +0700

    ui fina;l

--- Dosen.py
+++ Dosen.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from prettytable import PrettyTable
 from Database.Connect import Connect
 
 class Jadwal:
@@ -9,10 +10,20 @@ class Jadwal:
         self.nama=Result["NAMA"]
         self.matakuliah=Result["MATAKULIAH"]
 
-    def ExamSchedule(self):
+    # def ExamSchedule(self,Time,date):
+    #     print(f"Halo MR/MRS {self.nama}")
+    #     Date=input("Silakan masukan tanggal ujian(YYYY-MM-DD): ")
+    #     Time=input("Silakan masukan Waktu ujian(HH:MM-HH:MM): ")
+    #     self.__db.insert(
+    #         'jadwal',
+    #         {"ID":"Null", 
+    #         "DOSEN":f"{self.nama}",
+    #         "MATAKULIAH":f"{self.matakuliah}",
+    #         "JAM":f"{Time}",
+    #         "TANGGAL":f"{Date}"}
+    #         )
+    def ExamSchedule(self,Time,Date):
         print(f"Halo MR/MRS {self.nama}")
-        Date=input("Silakan masukan tanggal ujian(YYYY-MM-DD): ")
-        Time=input("Silakan masukan Waktu ujian(HH:MM-HH:MM): ")
         self.__db.insert(
             'jadwal',
             {"ID":"Null", 
@@ -22,20 +33,17 @@ class Jadwal:
             "TANGGAL":f"{Date}"}
             )
         
-
         
     def DisplayExam(self):
-        x=self.__db.select("jadwal")
-        for i in range(len(x)):
-            print(f"{i+1}. {x[i]}")
+        data = self.__db.select("jadwal")
+        table = PrettyTable()
+        table.field_names = data[0].keys()
+        for row in data:
+            table.add_row(row.values())
+        return table
 
-
-    def UpdateExam(self):
-        self.DisplayExam()
+    def UpdateExam(self,x,Time,Date):
         print(f"Halo MR/MRS {self.nama}, Silakan melakukan perubahan jadwal dimohon untuk tidak mengaganngu jadwal dosen lain")
-        x=input("Silakan masukan ID: ")
-        Date=input("Silakan masukan tanggal ujian(YYYY-MM-DD): ")
-        Time=input("Silakan masukan Waktu ujian(HH:MM-HH:MM): ")
         self.__db.update(
             "jadwal",
             {"ID":f"{x}"},
@@ -43,37 +51,52 @@ class Jadwal:
             "TANGGAL":f"{Date}"}
         )
 
-    def DeleteExam(self):
-        self.DisplayExam()
+    # def UpdateExam(self,x,Time,Date):
+    #     print(f"Halo MR/MRS {self.nama}, Silakan melakukan perubahan jadwal dimohon untuk tidak mengaganngu jadwal dosen lain")
+    #     x=input("Silakan masukan ID: ")
+    #     Date=input("Silakan masukan tanggal ujian(YYYY-MM-DD): ")
+    #     Time=input("Silakan masukan Waktu ujian(HH:MM-HH:MM): ")
+    #     self.__db.update(
+    #         "jadwal",
+    #         {"ID":f"{x}"},
+    #         {"JAM":f"{Time}",
+    #         "TANGGAL":f"{Date}"}
+    #     )
+
+    def DeleteExam(self,x):
         print(f"Halo MR/MRS {self.nama}, Silakan melakukan penghapusan jadwal dimohon untuk tidak mengaganngu jadwal dosen lain")
-        x=input("Silakan masukan ID: ")
         self.__db.delete("jadwal",{"ID":f"{x}"})
 
-while True:
-    try:
-        id=input("Silakan masukan id dosen: ")
-        dosen=Jadwal(id)
-        break
-    except:
-        print("Input eror masukan id dosen dengan benar")
+    # def DeleteExam(self,x):
+    #     print(f"Halo MR/MRS {self.nama}, Silakan melakukan penghapusan jadwal dimohon untuk tidak mengaganngu jadwal dosen lain")
+    #     x=input("Silakan masukan ID: ")
+    #     self.__db.delete("jadwal",{"ID":f"{x}"})
 
-while True:
-    y=["Buat Jadwal","Update Jadwal", "Melihat Jadwal", "Hapus Jadwal", "Keluar"]
-    for i in range(len(y)):
-        print(f"{i+1}. {y[i]}")
-    x=input(f"Halo mr/mrs {dosen.nama} silakan masukan input nomor: ")
-    if x == "1":
-        dosen.ExamSchedule()
-    elif x == "2":
-        dosen.UpdateExam()
-    elif x == "3":
-        dosen.DisplayExam()
-    elif x == "4":
-        dosen.DeleteExam()
-    elif x == "5":
-        print("Terimakasih")
-        break
-    else:
-        print("Input eror silakan masukan ulang")
+# while True:
+#     try:
+#         id=input("Silakan masukan id dosen: ")
+#         dosen=Jadwal(id)
+#         break
+#     except:
+#         print("Input eror masukan id dosen dengan benar")
+
+# while True:
+#     y=["Buat Jadwal","Update Jadwal", "Melihat Jadwal", "Hapus Jadwal", "Keluar"]
+#     for i in range(len(y)):
+#         print(f"{i+1}. {y[i]}")
+#     x=input(f"Halo mr/mrs {dosen.nama} silakan masukan input nomor: ")
+#     if x == "1":
+#         dosen.ExamSchedule()
+#     elif x == "2":
+#         dosen.UpdateExam()
+#     elif x == "3":
+#         dosen.DisplayExam()
+#     elif x == "4":
+#         dosen.DeleteExam()
+#     elif x == "5":
+#         print("Terimakasih")
+#         break
+#     else:
+#         print("Input eror silakan masukan ulang")
 
 
--- ui.py
+++ ui.py
@@ -1,62 +1,69 @@
-from tkinter import *
-from tkinter.messagebox import showinfo
+import customtkinter as tk
+import tkinter
+from Dosen import *
 
-# Fungsi Operasi
-def hitung_operasi():
-    try:
-        Bil1_VALL = float(Bil1.get())
-        Bil2_VALL = float(Bil2.get())
+tk.set_appearance_mode("dark")
+tk.set_default_color_theme("blue")
 
-        operasi = var_operasi.get()
+app=tk.CTk()
+app.geometry("600x600")
 
-        if operasi == "Penjumlahan":
-            hasil = Bil1_VALL + Bil2_VALL
-        elif operasi == "Pengurangan":
-            hasil = Bil1_VALL - Bil2_VALL
-        elif operasi == "Perkalian":
-            hasil = Bil1_VALL * Bil2_VALL
-        elif operasi == "Pembagian":
-            if Bil2_VALL != 0:
-                hasil = Bil1_VALL / Bil2_VALL
-            else:
-                hasil = "Error (Pembagian oleh 0)"
-        else:
-            hasil = "Pilih operasi"
 
-        label_hasil.config(text="Hasil: " + str(hasil))
 
-    except ValueError:
-        label_hasil.config(text="Masukkan bilangan yang valid")
+tabView = tk.CTkTabview(app)
+tabView.configure(width=500)
+tabView.pack(padx=0, pady=0)
 
-# Membuat window
-windowUtama = Tk()
-windowUtama.title("Kalkulator")
-windowUtama.geometry("450x350+550+100")
+tabView.add("Tambah Jadwal")
+tabView.add("Update Jadwal")
+tabView.add("Delete Jadwal")
 
-# Membuat entry
-label_bil1 = Label(windowUtama, text="Bilangan 1:")
-label_bil1.pack(pady=(50, 0))
-Bil1 = Entry(windowUtama)
-Bil1.pack(pady=(0, 10))
+def BuatJadwal():
+    dosen.ExamSchedule(TambahTime.get(),TambahDate.get())
 
-label_bil2 = Label(windowUtama, text="Bilangan 2:")
-label_bil2.pack(pady=(10, 0))
-Bil2 = Entry(windowUtama)
-Bil2.pack(pady=(0, 10))
+def UpdateJadwal():
+    dosen.ExamSchedule(Updateid.get(),UpdateTime.get(),UpdateDate.get())
 
-# Membuat Pilihan Operasi
-label_operasi = Label(windowUtama, text="Pilih Operasi:")
-label_operasi.pack()
-var_operasi = StringVar()
-var_operasi.set("Penjumlahan")
-operasi_menu = OptionMenu(windowUtama, var_operasi, "Penjumlahan", "Pengurangan", "Perkalian", "Pembagian")
-operasi_menu.pack(pady=(0, 10))
+def DeleteJadwal():
+    dosen.DeleteExam(Deleteid.get())
 
-# Membuat button
-tombol1 = Button(windowUtama, text='Submit', command=hitung_operasi)
-tombol1.pack()
 
-label_hasil = Label(windowUtama, text="Hasil: ")
-label_hasil.pack()
+#Tab Tambah Jadwal
+TambahTime = tk.CTkEntry(tabView.tab("Tambah Jadwal"), placeholder_text="Masukan Waktu ujian(HH:MM-HH:MM)")
+TambahTime.pack(padx=0, pady=0)
+TambahTime.configure(width=300)
+TambahDate = tk.CTkEntry(tabView.tab("Tambah Jadwal"), placeholder_text="Masukan tanggal ujian(YYYY-MM-DD)")
+TambahDate.pack(padx=0, pady=0)
+TambahDate.configure(width=300)
+Tambahsumbit=tk.CTkButton(tabView.tab("Tambah Jadwal"), text="Sumbit", command=BuatJadwal)
+Tambahsumbit.pack(padx=0, pady=0)
 
-windowUtama.mainloop()
+#Tab Update Jadwal
+Updateid = tk.CTkEntry(tabView.tab("Update Jadwal"), placeholder_text="Masukan id")
+Updateid.pack(padx=0, pady=0)
+Updateid.configure(width=300)
+UpdateTime = tk.CTkEntry(tabView.tab("Update Jadwal"), placeholder_text="Masukan Waktu (HH:MM-HH:MM)")
+UpdateTime.pack(padx=0, pady=0)
+UpdateTime.configure(width=300)
+UpdateDate = tk.CTkEntry(tabView.tab("Update Jadwal"), placeholder_text="Masukan tanggal (YYYY-MM-DD)")
+UpdateDate.pack(padx=0, pady=0)
+UpdateDate.configure(width=300)
+Updatesumbit=tk.CTkButton(tabView.tab("Update Jadwal"), text="Sumbit", command=UpdateJadwal)
+Updatesumbit.pack(padx=0, pady=0)
+
+#Tab Delete Jadwal
+Deleteid = tk.CTkEntry(tabView.tab("Delete Jadwal"), placeholder_text="Masukan id")
+Deleteid.pack(padx=0, pady=0)
+Deleteid.configure(width=300)
+Deletesumbit=tk.CTkButton(tabView.tab("Delete Jadwal"), text="Sumbit", command=DeleteJadwal)
+Deletesumbit.pack(padx=0, pady=0)
+
+text1=tk.CTkTextbox(app)
+text1.configure(width=500)
+text1.pack(padx = 0, pady =0)
+dosen=Jadwal(1)
+text=dosen.DisplayExam()
+
+text1.insert("0.0",text)
+
+app.mainloop()
